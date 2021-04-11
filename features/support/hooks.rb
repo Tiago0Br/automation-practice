c2390f81895de8d214b97119217c735cc036a53f
Tiago Tavares Lopes <tiagoltavares2002@gmail.com>
2021-04-11 17:56:12 -0300

    Adicionando hook para efetuar login

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,5 +1,20 @@
-Before do
-    page.current_window.resize_to(1366, 768)
+Before('@login') do
+    usuario = YAML.load_file(File.join(Dir.pwd, "features/support/config/login.yaml"))
+    visit "/"
+    click_on "Sign in"
+    login_with(usuario['login'], usuario['password'])
+end
+
+After do |scenario|
+    if scenario.failed?
+        screenshot = page.save_screenshot("logs/error.png")
+
+        Allure.add_attachment(
+            name: "Error",
+            type: Allure::ContentType::PNG,
+            source: File.open(screenshot)
+        )
+    end
 end
 
 Before('@comprar') do
