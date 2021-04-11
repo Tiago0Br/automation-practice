d19779173c23fc253528f5ec20e0bada9ef197e9
Tiago Tavares Lopes <tiagoltavares2002@gmail.com>
2021-04-11 17:57:20 -0300

    Adicionando configuração para maximizar a tela

--- features/support/env.rb
+++ features/support/env.rb
@@ -13,6 +13,7 @@ Capybara.configure do |config|
     config.default_max_wait_time = 10
     config.app_host = "http://automationpractice.com"
 end
+Capybara.page.driver.browser.manage.window.maximize
 
 AllureCucumber.configure do |config|
     config.results_directory = "/logs"
