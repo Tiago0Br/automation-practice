b5104dcf21edc54e9bbc292230c82a03f749f455
Tiago Tavares Lopes <tiagoltavares2002@gmail.com>
2021-04-17 20:29:03 -0300

    Adicionando page objects para o cenário de visualizar produtos

--- features/pages/listagemProdutos.rb
+++ features/pages/listagemProdutos.rb
@@ -9,12 +9,20 @@ class ListagemProdutos
         Review.new
     end
 
+    def compartilhar
+        ShareModal.new
+    end
+
     def lista_produtos
         all(".product_list a.product-name")
     end
 
     def selecionar_produto(produto)
         find("a[class='product-name']", text: produto).click
+    end
+
+    def nome_produto
+        find("h1[itemprop='name']").text
     end
 
     def msg_produto_nao_encontrado
--- features/step_definitions/visualizar_produto_spec.rb
+++ features/step_definitions/visualizar_produto_spec.rb
@@ -4,9 +4,9 @@ Dado('que estou na pagina inicial') do
 end
   
 Quando('eu clico no produto {string}') do |produto|
-    find("a[class='product-name']", text: produto).click
+    @listagemProdutos.selecionar_produto(produto)
 end
   
 Entao('devo visualizar o {string} com mais detalhes') do |produto|
-    expect(find("h1[itemprop='name']").text).to eql produto
+    expect(@listagemProdutos.nome_produto).to eql produto
 end
